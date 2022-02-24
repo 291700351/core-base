47953a93d893a400f0d9add82970ffbf87948550
Ice Lee <lb291700351@live.cn>
2022-02-24 18:10:36 +0800

    Fix database

--- app/src/main/java/io/github/lee/tmdb/global/di/database/AppDatabase.kt
+++ app/src/main/java/io/github/lee/tmdb/global/di/database/AppDatabase.kt
@@ -2,12 +2,17 @@ package io.github.lee.tmdb.global.di.database
 
 import androidx.annotation.Keep
 import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import androidx.room.RoomDatabase
 
+@Keep
+@Entity
+data class Test(@PrimaryKey val id: Long)
 
 @Keep
 @Database(
-    entities = [],
+    entities = [Test::class],
     version = 1,
     exportSchema = false
 )
